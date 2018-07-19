bba141a4b8772e9827a9634ee83fd926251cf582
chi.thi.thuy.hang@framgia.com <chu.thi.thuy.hang@framgia.com>
2018-07-19 20:08:46 +0700

    commit in develop

---
+++ database/migrations/2018_07_20_021749_create_comments_table.php
@@ -0,0 +1,22 @@
+<?php
+    use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Database\Migrations\Migration;
+    class CreateCommentsTable extends Migration
+    {
+        public function up()
+        {
+            Schema::create('comments', function (Blueprint $table) {
+                $table->increments('id');
+                $table->text('content');
+                $table->integer('post_id');
+                $table->integer('user_id')->nullable();
+                $table->tinyInteger('status')->default(1);
+                $table->timestamps();
+            });
+        }
+        public function down()
+        {
+            Schema::dropIfExists('comments');
+        }
+    }
